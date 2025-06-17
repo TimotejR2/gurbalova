9e4880d3c0594559224edfd4de79c613864ae5a8
TimotejR <timotej.ruzicka@gmail.com>
2025-06-17 20:59:17 +0200

    asf

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/<path:path>', methods=['GET'])
+def serve_static(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def home():
     return send_from_directory('html', 'index.html')
-
-@app.route('/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
