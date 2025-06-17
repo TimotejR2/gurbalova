f6ff14068afc590217c2b1e7c103c15dbaf70882
TimotejR <timotej.ruzicka@gmail.com>
2025-06-17 21:23:07 +0200

    asf

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/<path:path>', methods=['GET'])
+@app.route('/static/<path:path>', methods=['GET'])
 def serve_static(path):
     return send_from_directory('static', path)
 
